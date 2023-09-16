f9922906cf2cdd1ac2653375b8b6e3f2ab1be517
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-16 20:18:28 +0530

    Add Menu page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Routes
 import Home from "./pages/Home";
+import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
---
+++ src/pages/Menu.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/layout/Layout/Layout";
+
+const Menu = () => {
+  return (
+    <Layout>
+      <h1>Menu Page</h1>
+    </Layout>
+  );
+};
+
+export default Menu;
