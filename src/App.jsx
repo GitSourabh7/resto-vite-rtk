b8f15841cb57b58c8668e9b1ae70c552b4d815cb
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-16 19:01:19 +0530

    Import Layout to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Layout from "./components/layout/Layout/Layout";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
+      <Layout></Layout>
       <Home />
     </>
   );
---
+++ src/components/layout/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
