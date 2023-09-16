5a994675e308cab1c958f327b3d21f7d77b708b8
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-16 18:43:12 +0530

    Update Home page
    Add Title, tag line and Order Now button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
-function Home() {
-  return <div>Home</div>;
-}
+import "../styles/HomeStyles.css";
+const Banner = `https://res.cloudinary.com/dduqxunnf/image/upload/v1690220780/resto/banner.jpg`;
+const Home = () => {
+  return (
+    <div
+      className="home"
+      style={{
+        backgroundImage: `url(${Banner})`,
+      }}
+    >
+      <div className="headerContainer">
+        <h1>Resto</h1>
+        <p id="typing">Best food in India</p>
+        <button>Order Now</button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
