9234a5dc0fa10b211f76a0f3e32df8e09cb09daf
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-16 19:38:48 +0530

    Update Header

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,7 +1,120 @@
+import { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { NavLink } from "react-router-dom";
+import "./HeaderStyle.css";
+// const Logo = `https://res.cloudinary.com/dduqxunnf/image/upload/v1690208310/resto/logo/resto_logo.svg`;
+import Logo from "../../../assets/logo.svg";
+
 const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  //handle menu click
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  //Menu Drawer
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography
+        color={"goldenrod"}
+        variant="h6"
+        component="div"
+        sx={{ flexGrow: 1, my: 2 }}
+      >
+        <img src={Logo} alt="logo" height={"45"} width="140" />
+      </Typography>
+
+      {/* This divider gets displayed when drawer is expanded */}
+      <Divider />
+
+      <ul className="mobile-navigation">
+        <li>
+          <NavLink activeclassname="active" to={"/"}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/menu"}>Menu</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/about"}>About</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/contact"}>Contact</NavLink>
+        </li>
+      </ul>
+    </Box>
+  );
+
   return (
     <>
-      <h1>Header</h1>
+      <Box>
+        <AppBar component={"nav"} sx={{ bgcolor: "#1976d2" }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{ mr: 2, display: { sm: "none" } }}
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              color={"goldenrod"}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: "flex" }}
+            >
+              <img src={Logo} alt="logo" height={"45"} width="150" />
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <ul className="navigation-menu">
+                <li>
+                  <NavLink activeclassname="active" to={"/"}>
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/menu"}>Menu</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/about"}>About</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/contact"}>Contact</NavLink>
+                </li>
+              </ul>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": { boxSizing: "border-box", width: "240px" },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box>
+          <Toolbar />
+        </Box>
+      </Box>
     </>
   );
 };
