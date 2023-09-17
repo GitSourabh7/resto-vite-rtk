953bf04ca413040893ad19d4fd25800c1049aff2
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-17 22:05:07 +0530

    Update Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import Layout from "../components/layout/Layout";
 import {
   Box,
+  Link,
   Paper,
   Table,
   TableBody,
@@ -32,12 +33,18 @@ const Contact = () => {
             "& h4": { fontWeight: "bold", mb: 2 },
           }}
         >
-          <Typography variant="h4">Contact my Restaurant</Typography>
           <Typography sx={{ fontSize: "1.2rem" }}>
-            Pari atur adversarial examples quis enim quis VAEs irure chatbots
-            machine learning feature extraction AI in entertainment adversarial
-            examples Dolor neural architecture search sit ETL principal
-            component analysis deserunt voluptate culpa.
+            At Resto, we&apos;re committed to providing you with the best dining
+            experiences around the world. Your feedback, questions, and
+            suggestions are important to us. Feel free to get in touch with our
+            dedicated team through the following channels:
+          </Typography>
+          <br />
+          <Typography variant="h4">Customer Support:</Typography>
+          <Typography sx={{ fontSize: "1.2rem", marginTop: "-1rem" }}>
+            Our customer support team is available 24/7 to assist you with any
+            inquiries, reservations, or concerns you may have. Don&apos;t
+            hesitate to reach out:
           </Typography>
         </Box>
         <Box
@@ -71,7 +78,13 @@ const Contact = () => {
                   >
                     <SupportAgentIcon sx={{ color: "red", fontSize: 40 }} />
                     <Typography sx={{ pl: 2, fontSize: 20 }}>
-                      1800 12 3456 (Toll-Free)
+                      <Link
+                        sx={{ textDecoration: "none" }}
+                        href="tel:1800 12 3456"
+                      >
+                        1800 12 3456
+                      </Link>
+                      <em> (Toll-Free)</em>
                     </Typography>
                   </TableCell>
                 </TableRow>
@@ -81,7 +94,7 @@ const Contact = () => {
                   >
                     <MailIcon sx={{ color: "skyblue", fontSize: 40 }} />
                     <Typography sx={{ pl: 2, fontSize: 20 }}>
-                      help@resto.com
+                      <Link href="mailto:help@resto.com">help@resto.com</Link>
                     </Typography>
                   </TableCell>
                 </TableRow>
@@ -91,7 +104,12 @@ const Contact = () => {
                   >
                     <CallIcon sx={{ color: "green", fontSize: 40 }} />
                     <Typography sx={{ pl: 2, fontSize: 20 }}>
-                      +91 9876543210
+                      <Link
+                        sx={{ textDecoration: "none" }}
+                        href="tel:+91 9876543210"
+                      >
+                        +91 9876543210
+                      </Link>
                     </Typography>
                   </TableCell>
                 </TableRow>
@@ -101,13 +119,82 @@ const Contact = () => {
                   >
                     <AccessTimeIcon sx={{ color: "violet", fontSize: 40 }} />
                     <Typography sx={{ pl: 2, fontSize: 20 }}>
-                      10.00 am to 10.00 pm
+                      10.00 am to 10.00 pm <em>(Office Hours)</em>
                     </Typography>
                   </TableCell>
                 </TableRow>
               </TableBody>
             </Table>
           </TableContainer>
+        </Box>
+        <Box
+          sx={{
+            my: 5,
+            mx: "5%",
+            display: "flex",
+            flexDirection: "column",
+            "& h4": { fontWeight: "bold", mb: 2 },
+          }}
+        >
+          <Typography variant="h4">Restaurant Partners:</Typography>
+          <Typography sx={{ fontSize: "1.2rem", marginTop: "-1rem" }}>
+            If you&apos;re a restaurant owner interested in partnering with
+            Resto, or if you have any business-related inquiries, please contact
+            our restaurant partnership team:
+            <Typography sx={{ fontSize: "1.2rem" }}>
+              Email:
+              <Link
+                sx={{ textDecoration: "none" }}
+                href="mailto:partners@domain.com"
+              >
+                partners@domain.com
+              </Link>
+              , Phone:
+              <Link sx={{ textDecoration: "none" }} href="tel:+91 9876543210">
+                +91 9876543210
+              </Link>
+            </Typography>
+          </Typography>
+          <br />
+          <Typography variant="h4"> Feedback and Suggestions: </Typography>
+          <Typography sx={{ fontSize: "1.2rem", marginTop: "-1rem" }}>
+            We value your feedback and suggestions as they help us improve our
+            services. If you have any ideas, comments, or recommendations,
+            please send them to: Email:{" "}
+            <Link sx={{ textDecoration: "none" }} href="feedback@resto.com">
+              feedback@resto.com
+            </Link>
+          </Typography>
+          <br />
+          <Typography variant="h4"> Visit Us: </Typography>
+          <Typography sx={{ fontSize: "1.2rem", marginTop: "-1rem" }}>
+            If you&apos;re in the neighborhood, we&apos;d love to meet you in
+            person. Here&apos;s our headquarters&apos;
+            <div>
+              <Typography
+                variant="h6"
+                sx={{ fontSize: "1.2rem", fontWeight: "bold" }}
+              >
+                Our Address
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{ fontSize: "1.2rem", fontStyle: "italic" }}
+              >
+                123 Main Street,
+                <br />
+                Bangalore,
+                <br />
+                Karnataka, 560 560
+              </Typography>
+            </div>
+          </Typography>
+          <br />
+          <Typography sx={{ fontSize: "1.2rem" }}>
+            Resto is dedicated to providing exceptional dining experiences, and
+            we&apos;re here to assist you in any way we can. Thank you for
+            choosing Resto, where global flavors meet your plate.
+          </Typography>
         </Box>
       </Box>
     </Layout>
