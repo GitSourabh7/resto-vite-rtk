b36c931a28c2f398bb9400623c9de7169883384e
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-17 17:52:45 +0530

    Update MenuCard

--- src/components/menu/Card/MenuCard.jsx
+++ src/components/menu/Card/MenuCard.jsx
@@ -1,85 +1,70 @@
+/* eslint-disable react/prop-types */
 import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
 import CardButtons from "./CardButtons";
 
-var menuList = [
-  {
-    id: 1,
-    name: "Tempura",
-    category: "japanese",
-    description: "This dish is very delicious and very famous in Japan.",
-    image:
-      "https://res.cloudinary.com/dduqxunnf/image/upload/v1690219271/resto/menu/japanese/tempura.jpg",
-    price: 200,
-  },
-  {
-    id: 2,
-    name: "Ramen",
-    category: "japanese",
-    description: "This dish is very delicious and very famous in Japan.",
-    image:
-      "https://res.cloudinary.com/dduqxunnf/image/upload/v1690219188/resto/menu/japanese/ramen.jpg",
-    price: 300,
-  },
-  {
-    id: 3,
-    name: "Sushi",
-    category: "japanese",
-    description: "This dish is very delicious and very famous in Japan.",
-    image:
-      "https://res.cloudinary.com/dduqxunnf/image/upload/v1690219153/resto/menu/japanese/sushi.jpg",
-    price: 50,
-  },
-];
-const MenuCard = () => {
-  return menuList.map((menu) => (
-    <Card
-      key={menu.name} // Used name as key
+const MenuCard = ({ menuList }) => {
+  return (
+    <Box
       sx={{
-        maxWidth: "400px",
         display: "flex",
-        m: 2,
-        borderRadius: "10px",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        paddingLeft: "100px",
+        height: "460px",
+        overflowY: "scroll",
       }}
     >
-      <CardContent
-        sx={{
-          marginBottom: "-35px",
-          "&:hover": {
-            background: "#9c96960f",
-          },
-        }}
-      >
-        <CardMedia
-          sx={{ minHeight: "400px", borderRadius: "20px" }}
-          component={"img"}
-          src={menu.image}
-          alt={menu.name}
-        />
-        <Box
+      {menuList.map((menu) => (
+        <Card
+          key={menu.name} // Used name as key
           sx={{
-            textAlign: "center",
-            backgroundColor: "#c5ccc5c4",
-            padding: "15px",
-            position: "relative",
-            top: "-45px",
-            mx: "25px",
-            borderRadius: "30px",
+            maxWidth: "350px",
+            display: "flex",
+            m: 2,
+            borderRadius: "10px",
           }}
         >
-          <Typography
-            variant="h5"
-            gutterBottom
-            component={"div"}
-            sx={{ fontFamily: "cursive" }}
+          <CardContent
+            sx={{
+              marginBottom: "-75px",
+              "&:hover": {
+                background: "#9c96960f",
+              },
+            }}
           >
-            {menu.name}
-          </Typography>
-          <Typography variant="body2">{menu.description}</Typography>
-        </Box>
-        <CardButtons menu={menu} />
-      </CardContent>
-    </Card>
-  ));
+            <CardMedia
+              sx={{ minHeight: "300px", borderRadius: "20px" }}
+              component={"img"}
+              src={menu.image}
+              alt={menu.name}
+            />
+            <Box
+              sx={{
+                textAlign: "center",
+                backgroundColor: "#c5ccc5c4",
+                padding: "15px",
+                position: "relative",
+                top: "-50px",
+                mx: "25px",
+                borderRadius: "30px",
+              }}
+            >
+              <Typography
+                variant="h5"
+                gutterBottom
+                component={"div"}
+                sx={{ fontFamily: "cursive" }}
+              >
+                {menu.name}
+              </Typography>
+              <Typography variant="body2">{menu.description}</Typography>
+            </Box>
+            <CardButtons menu={menu} />
+          </CardContent>
+        </Card>
+      ))}
+    </Box>
+  );
 };
 
 export default MenuCard;
