db73fb660d8a48158e8cb37a97d63173c38eb02a
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-20 08:51:23 +0530

    Update totalPages count from Menu

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { setMenus } from "../menu/MenuSlice";
 
 //Mui
 import { Box } from "@mui/material";
@@ -8,7 +7,12 @@ import Layout from "../../components/layout/Layout";
 import CategoryFilter from "../../components/menu/CategoryFilter";
 import MenuCard from "../../components/menu/Card/MenuCard";
 import CartButton from "../../components/menu/Cart/CartButton";
-import PaginationComp from "../../components/menu/PaginationComp";
+import PaginationComp from "../../components/menu/Pagination/PaginationComp";
+
+// Slice Value
+import { setMenus } from "../menu/MenuSlice";
+import { setTotalPages } from "../../components/menu/Pagination/PaginationSlice";
+import { store } from "../../store/store";
 
 //styles
 import "../menu/MenuStyle.css";
@@ -203,6 +207,11 @@ const Menu = () => {
     // Simulate fetching menu data from an API
     setTimeout(() => {
       dispatch(setMenus(menuData));
+      dispatch(
+        setTotalPages(
+          Math.ceil(menuData.length / store.getState().pagination.itemsPerPage)
+        )
+      );
     }, 1000);
   }, [dispatch]);
 
