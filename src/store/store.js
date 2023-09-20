87808960c1707dcfa9981ac9522257526b9f4678
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-20 08:47:24 +0530

    Use paginationReducer in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from "../pages/menu/MenuSlice";
+import paginationReducer from "../components/menu/Pagination/PaginationSlice";
 import logger from "redux-logger";
 
 export const store = configureStore({
   reducer: {
     menu: menuReducer,
+    pagination: paginationReducer,
   },
   middleware: [logger],
 
