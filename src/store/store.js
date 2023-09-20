6d563b95a6063c20010515b29c27c4ea3fb3a8c0
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-20 23:26:45 +0530

    Add cartReducer in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from "../pages/menu/MenuSlice";
 import paginationReducer from "../components/menu/Pagination/PaginationSlice";
+import cartReducer from "../components/menu/Cart/CartSlice";
 import logger from "redux-logger";
 
 export const store = configureStore({
   reducer: {
     menu: menuReducer,
     pagination: paginationReducer,
+    cart: cartReducer,
   },
   middleware: [logger],
 
