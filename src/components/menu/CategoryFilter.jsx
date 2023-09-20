a0403a1c6fde698664f0dece67de3fb36ad991b7
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-20 08:49:32 +0530

    fetch data from api in CategoryFilter

--- src/components/menu/CategoryFilter.jsx
+++ src/components/menu/CategoryFilter.jsx
@@ -1,24 +1,50 @@
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  filterByCategory,
-  selectSelectedCategory,
-} from "../../pages/menu/MenuSlice";
 
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 
-const categories = ["japanese", "indian", "chinese", "italian"];
+import {
+  filterByCategory,
+  selectSelectedCategory,
+} from "../../pages/menu/MenuSlice";
+import { setTotalPages, setCurrentPage } from "./Pagination/PaginationSlice";
+import { store } from "../../store/store";
 
 const CategoryFilter = () => {
   const selectedCategory = useSelector(selectSelectedCategory);
   const dispatch = useDispatch();
 
+  const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate fetching menu data from an API
+    setTimeout(() => {
+      fetch("http://localhost:8000/categories.json")
+        .then((response) => response.json())
+        .then((data) => {
+          setCategories(data);
+          setIsLoading(false);
+        });
+    }, 1000);
+  });
+
   // Function to handle button clicks and set the active button
   const handleCategoryClick = (category) => {
     dispatch(filterByCategory(category));
+
+    const newTotalPages = Math.ceil(
+      store.getState().menu.filteredMenus.length /
+        store.getState().pagination.itemsPerPage
+    );
+    dispatch(setTotalPages(newTotalPages));
+    dispatch(setCurrentPage(1));
   };
 
-  return (
+  return isLoading ? (
+    <div>Loading categories...</div>
+  ) : (
     <ButtonGroup
       orientation="vertical"
       aria-label="vertical outlined button group"
