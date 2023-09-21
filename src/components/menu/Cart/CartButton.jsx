80212929ff6c97280b6a594964f46db7e9b9622f
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-21 09:23:18 +0530

    Add store value to CartList

--- src/components/menu/Cart/CartButton.jsx
+++ src/components/menu/Cart/CartButton.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useSelector } from "react-redux";
 
 // Components
 import {
@@ -28,29 +29,10 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-var stateValue = [
-  {
-    id: 1,
-    name: "Tempura",
-    category: "japanese",
-    description: "This dish is very delicious and very famous in Japan.",
-    image:
-      "https://res.cloudinary.com/dduqxunnf/image/upload/v1690219271/resto/menu/japanese/tempura.jpg",
-    price: 200,
-  },
-  {
-    id: 2,
-    name: "Ramen",
-    category: "japanese",
-    description: "This dish is very delicious and very famous in Japan.",
-    image:
-      "https://res.cloudinary.com/dduqxunnf/image/upload/v1690219188/resto/menu/japanese/ramen.jpg",
-    price: 300,
-  },
-];
-
 export default function CartButton() {
   const [open, setOpen] = React.useState(false);
+
+  const cartItems = useSelector((state) => state.cart.items);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -62,7 +44,7 @@ export default function CartButton() {
   return (
     <div>
       <Badge
-        badgeContent={stateValue ? stateValue.length : 0}
+        badgeContent={cartItems ? cartItems.length : 0}
         color="primary"
         sx={{ position: "fixed", my: 30, mx: "6rem", zIndex: 1 }}
       ></Badge>
@@ -97,10 +79,10 @@ export default function CartButton() {
         </AppBar>
         <Box sx={{ display: "flex", flexDirection: "row" }}>
           <Box sx={{ display: "flex", flexDirection: "column", width: "80%" }}>
-            <CartHeader count={stateValue.length} />
-            <CartItemList itemList={stateValue} />
+            <CartHeader count={cartItems.length} />
+            <CartItemList itemList={cartItems} />
           </Box>
-          <CartSummary cartItems={stateValue} />
+          <CartSummary cartItems={cartItems} />
         </Box>
       </Dialog>
     </div>
