148b42174d8bf99ca3a44608840ab229e4f56dbf
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-24 12:50:57 +0530

    Update CartItemList
    import QuantityInput

--- src/components/menu/Cart/CartItemList.jsx
+++ src/components/menu/Cart/CartItemList.jsx
@@ -5,6 +5,7 @@ import { Box, Button, Typography } from "@mui/material";
 import { Delete } from "@mui/icons-material";
 
 import { removeFromCart } from "./CartSlice";
+import QuantityInput from "./QuantityInput";
 
 const CartItemList = ({ cartItems }) => {
   const dispatch = useDispatch();
@@ -24,72 +25,88 @@ const CartItemList = ({ cartItems }) => {
     >
       {/* Item List */}
       {cartItems.length ? (
-        cartItems.map((menu) => (
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              bgcolor: "#00000022",
-              mx: "10px",
-              my: "1px",
-              borderRadius: "5px",
-            }}
-            key={menu.id}
-          >
-            <Box
-              component="img"
-              sx={{
-                m: 2,
-                borderRadius: "10px",
-                height: 120,
-                width: 180,
-              }}
-              alt={menu.name}
-              src={menu.image}
-            />
+        cartItems.map((menu) => {
+          const totalPrice = menu.price * menu.quantity; // Calculate the total price
+
+          return (
             <Box
               sx={{
                 display: "flex",
-                justifyContent: "space-between",
                 alignItems: "center",
-                margin: "10px",
-                width: "-webkit-fill-available",
+                bgcolor: "#00000022",
+                mx: "10px",
+                my: "1px",
+                borderRadius: "5px",
               }}
+              key={menu.id}
             >
-              <Typography
+              <Box
+                component="img"
                 sx={{
-                  fontSize: "1.2em",
-                  fontWeight: "400",
-                  minWidth: "150px",
+                  m: 2,
+                  borderRadius: "10px",
+                  height: 120,
+                  width: 180,
                 }}
-              >
-                {menu.name}
-              </Typography>
+                alt={menu.name}
+                src={menu.image}
+              />
               <Box
                 sx={{
                   display: "flex",
+                  justifyContent: "space-between",
                   alignItems: "center",
-                  justifyContent: "space-around",
-                  mx: 2,
-                  width: "200px",
+                  margin: "10px",
+                  width: "-webkit-fill-available",
                 }}
               >
-                <Box sx={{ ml: "5", fontSize: "1.2em", fontWeight: "400" }}>
-                  ₹ {menu.price}
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <Typography
+                    sx={{
+                      fontSize: "1.2em",
+                      fontWeight: "400",
+                      minWidth: "100px",
+                    }}
+                  >
+                    {menu.name}
+                  </Typography>
+                  <QuantityInput sx={{ maxWidth: "80px" }} menu={menu} />
                 </Box>
-                <Button
-                  variant="outlined"
-                  startIcon={<Delete />}
-                  color="error"
-                  size="small"
-                  onClick={() => handleRemove(menu)}
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-around",
+                    mx: 2,
+                    minWidth: "300px",
+                  }}
                 >
-                  Remove
-                </Button>
+                  <Typography
+                    sx={{
+                      fontSize: "1em",
+                      fontWeight: "400",
+                      fontStyle: "italic",
+                    }}
+                  >
+                    ₹ {menu.price}/dish
+                  </Typography>
+                  <Box sx={{ ml: "5", fontSize: "1.2em", fontWeight: "400" }}>
+                    ₹ {totalPrice}
+                  </Box>
+                  <Button
+                    variant="outlined"
+                    startIcon={<Delete />}
+                    color="error"
+                    size="small"
+                    onClick={() => handleRemove(menu)}
+                  >
+                    Remove
+                  </Button>
+                </Box>
               </Box>
             </Box>
-          </Box>
-        ))
+          );
+        })
       ) : (
         <Box>
           <Typography variant="h4" sx={{ textAlign: "center" }}>
