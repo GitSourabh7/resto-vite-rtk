e5a1ac9993b83a6e89b0c44a47f1d1fbe0ad6075
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-27 00:41:52 +0530

    Update Login.jsx
    store jwt in localstorage

--- src/pages/Auth/LogIn.jsx
+++ src/pages/Auth/LogIn.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -21,14 +21,22 @@ export default function LogIn() {
     password: "",
   });
 
+  const [authenticated, setAuthenticated] = useState(false); // Track authentication state
+  const [jwtToken, setJwtToken] = useState(""); // Store JWT token
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
   const showToast = (message, type) => {
-    // Show a toast message with the specified message and type
     toast[type](message);
+  };
+
+  const handleLogout = () => {
+    // Implement logout logic here, e.g., clear JWT token and set authentication to false
+    setJwtToken("");
+    setAuthenticated(false);
   };
 
   const handleSubmit = async (event) => {
@@ -48,6 +56,8 @@ export default function LogIn() {
       if (response.status === 200) {
         // Successful login
         showToast("Login successful", "success");
+        setJwtToken(data.token); // Store the received JWT token
+        setAuthenticated(true); // Set authentication to true
         console.log("Login successful:", data);
       } else {
         // Handle login failure, e.g., display an error message
@@ -63,6 +73,22 @@ export default function LogIn() {
       console.error("Error during login:", error);
     }
   };
+
+  useEffect(() => {
+    // Check for stored JWT token on page load
+    const storedToken = localStorage.getItem("jwtToken");
+    if (storedToken) {
+      // Verify the token on the server (you need to implement this)
+      // If valid, set authentication to true
+      setAuthenticated(true);
+      setJwtToken(storedToken);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Store JWT token in local storage whenever it changes
+    localStorage.setItem("jwtToken", jwtToken);
+  }, [jwtToken]);
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -80,59 +106,70 @@ export default function LogIn() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Log in
+            {authenticated ? "Welcome" : "Log in"}
           </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 3 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={formData.email}
-              onChange={handleInputChange}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={formData.password}
-              onChange={handleInputChange}
-            />
+          {authenticated ? (
             <Button
-              type="submit"
+              onClick={handleLogout}
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+              Logout
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
+          ) : (
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 3 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/signup" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Link href="/signup" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
+            </Box>
+          )}
         </Box>
       </Container>
       <ToastContainer position="top-right" autoClose={5000} hideProgressBar />
