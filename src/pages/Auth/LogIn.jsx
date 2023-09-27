001aeaede1eb55f5302128cdf518d51042434fe6
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-27 23:40:40 +0530

    Remove logout function

--- src/pages/Auth/LogIn.jsx
+++ src/pages/Auth/LogIn.jsx
@@ -1,12 +1,8 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  setUser,
-  clearUser,
-  selectUser,
-} from "../../components/common/userSlice";
+import { setUser, selectUser } from "../../components/common/userSlice";
 import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
+import Button from "@mui/material/Button"; // Import Button from Material-UI
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import Link from "@mui/material/Link";
@@ -32,11 +28,6 @@ export default function LogIn() {
 
   const showToast = (message, type) => {
     toast[type](message);
-  };
-
-  const handleLogout = () => {
-    dispatch(clearUser());
-    localStorage.removeItem("jwtToken");
   };
 
   const handleSubmit = async (event) => {
@@ -108,72 +99,61 @@ export default function LogIn() {
               ? `Welcome, ${authenticated.firstName}`
               : "Log in"}
           </Typography>
-          {authenticated.firstName ? (
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 3 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={formData.password}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+            />
             <Button
-              onClick={handleLogout}
+              type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Logout
+              Sign In
             </Button>
-          ) : (
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 3 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                value={formData.email}
-                onChange={(e) =>
-                  setFormData({ ...formData, email: e.target.value })
-                }
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                value={formData.password}
-                onChange={(e) =>
-                  setFormData({ ...formData, password: e.target.value })
-                }
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="/signup" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
               </Grid>
-            </Box>
-          )}
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
         </Box>
       </Container>
       <ToastContainer position="top-right" autoClose={5000} hideProgressBar />
