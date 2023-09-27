824e47da92685a0abbac38f1e427ded0d58ec889
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-27 21:53:43 +0530

    Add userReducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from "../pages/menu/MenuSlice";
 import paginationReducer from "../components/menu/Pagination/PaginationSlice";
 import cartReducer from "../components/menu/Cart/CartSlice";
+import userReducer from "../components/common/userSlice";
 import logger from "redux-logger";
 
 export const store = configureStore({
@@ -9,6 +10,7 @@ export const store = configureStore({
     menu: menuReducer,
     pagination: paginationReducer,
     cart: cartReducer,
+    user: userReducer,
   },
   middleware: [logger],
 
