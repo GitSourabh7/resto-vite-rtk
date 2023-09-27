f81c5e618944b53b380ac14064675c02dd104941
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-27 23:40:10 +0530

    Save state to localStorage

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,22 @@ import cartReducer from "../components/menu/Cart/CartSlice";
 import userReducer from "../components/common/userSlice";
 import logger from "redux-logger";
 
+const saveState = (state) => {
+  const serializedState = JSON.stringify(state);
+  localStorage.setItem("state", serializedState);
+};
+
+const loadState = () => {
+  const serializedState = localStorage.getItem("state");
+  if (serializedState === null) {
+    return undefined;
+  }
+
+  return JSON.parse(serializedState);
+};
+
+const persistedState = loadState();
+
 export const store = configureStore({
   reducer: {
     menu: menuReducer,
@@ -13,10 +29,9 @@ export const store = configureStore({
     user: userReducer,
   },
   middleware: [logger],
+  preloadedState: persistedState,
+});
 
-  // Or (both works same)
-
-  // middleware: (getDefaultMiddleware) => {
-  //   return getDefaultMiddleware().concat(logger);
-  // },
+store.subscribe(() => {
+  saveState(store.getState());
 });
