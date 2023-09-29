eea304d0029bf8b0fe30a9886429f04e1eaae149
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-29 15:37:04 +0530

    Update Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,12 @@ import "../styles/HomeStyles.css";
 import Layout from "../components/layout/Layout";
 import { Link } from "react-router-dom";
 import "../components/common/typewriter";
+// import "./HomeStyle.css";
 
-const Banner = `https://res.cloudinary.com/dduqxunnf/image/upload/v1690220780/resto/banner.jpg`;
 const Home = () => {
   return (
     <Layout>
-      <div
-        className="home"
-        style={{
-          backgroundImage: `url(${Banner})`,
-        }}
-      >
+      <div className="home">
         <div className="headerContainer">
           <h1>Resto</h1>
           <p id="typing">Best food in India</p>
