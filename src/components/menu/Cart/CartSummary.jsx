0c5fffbe3012d75a7c13e054352f94926525f39d
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-29 15:59:08 +0530

    Update style of CartSummary

--- src/components/menu/Cart/CartSummary.jsx
+++ src/components/menu/Cart/CartSummary.jsx
@@ -2,38 +2,39 @@
 import { Box, Typography, Button } from "@mui/material";
 
 const CartSummary = ({ cartItems }) => {
-  const calculateSubTotal = () => {
-    let subTotal = 0;
-    for (let i = 0; i < cartItems.length; i++) {
-      subTotal += cartItems[i].price * cartItems[i].quantity;
-    }
-    return subTotal;
-  };
-
-  // Check if cartItems array is empty, and if so, don't render the component
   if (cartItems.length === 0) {
     return null;
   }
+
+  const calculateSubTotal = () => {
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  };
+
+  const deliveryCharges = cartItems.length !== 0 ? 80 : 0;
+  const subTotal = calculateSubTotal() + deliveryCharges;
 
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
-        bgcolor: "#00000022",
+        bgcolor: "#f0f0f0",
         margin: "10px",
         padding: 2,
         borderRadius: "5px",
-        width: "20%",
-        height: "220px",
+        width: "100%", // Use relative width
+        maxWidth: "300px", // Set a maximum width if needed
       }}
     >
       <Typography
+        variant="h6" // Use Typography variant
         sx={{
           mb: 2,
           textDecoration: "underline",
-          fontSize: "1.2em",
-          fontWeight: "800",
+          fontWeight: "bold",
         }}
       >
         Cart Summary
@@ -44,14 +45,12 @@ const CartSummary = ({ cartItems }) => {
       </Box>
       <Box sx={{ display: "flex", justifyContent: "space-between" }}>
         <Typography>Delivery Charges</Typography>
-        <Typography>₹ {cartItems.length !== 0 ? 80 : 0}</Typography>
+        <Typography>₹ {deliveryCharges}</Typography>
       </Box>
       <hr />
       <Box sx={{ display: "flex", justifyContent: "space-between" }}>
         <Typography>Subtotal</Typography>
-        <Typography>
-          ₹ {calculateSubTotal() + (cartItems.length !== 0 ? 80 : 0)}
-        </Typography>
+        <Typography>₹ {subTotal}</Typography>
       </Box>
       <Button variant="contained" sx={{ mt: 3 }}>
         Checkout
