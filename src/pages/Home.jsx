88a9461ab60a5bd70b340c2e3ac4657c665215c1
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-09-29 22:15:53 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,44 @@
+import { useEffect, useState } from "react";
 import "../styles/HomeStyles.css";
 import Layout from "../components/layout/Layout";
 import { Link } from "react-router-dom";
-import "../components/common/typewriter";
+import Typewriter from "typewriter-effect/dist/core";
 
 const Home = () => {
+  const [typingText] = useState("");
+
+  useEffect(() => {
+    const typewriter = new Typewriter("#typing", {
+      loop: true,
+      delay: 50,
+    });
+
+    typewriter
+      .typeString("Best global food restaurant in ")
+      .typeString(`<strong><span style="color: #1976d2;">India</span></strong>`)
+      .pauseFor(500)
+      .deleteChars(5)
+      .typeString(`<strong><span style="color: #1976d2;">China</span></strong>`)
+      .pauseFor(500)
+      .deleteChars(5)
+      .typeString(`<strong><span style="color: #1976d2;">Japan</span></strong>`)
+      .pauseFor(500)
+      .deleteChars(5)
+      .typeString(`<strong><span style="color: #1976d2;">Italy</span></strong>`)
+      .pauseFor(500)
+      .start();
+
+    return () => {
+      typewriter.stop();
+    };
+  }, []);
+
   return (
     <Layout>
       <div className="home">
         <div className="headerContainer">
           <h1>Resto</h1>
-          <p id="typing">Best food in India</p>
+          <p id="typing">{typingText}</p>
           <Link to="/menu">
             <button>Order Now</button>
           </Link>
