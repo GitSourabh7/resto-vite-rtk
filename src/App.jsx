d8cde9df7f168246756fcd895a77b06a8ddc37fc
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-10-02 17:41:16 +0530

    Update location of Menu page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LogIn from "./pages/Auth/LogIn";
 import SignUp from "./pages/Auth/SignUp";
 import Home from "./pages/Home";
-import Menu from "./pages/menu/Menu";
+import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import PageNotFound from "./pages/PageNotFound";
