d64d317ce5df7d70106fa7e4bcde800721afe0c8
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-10-02 15:35:12 +0530

    Use styled-components

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,53 +1,66 @@
+import styled from "styled-components";
 import { Box, Typography } from "@mui/material";
 import SocialIcons from "./SocialIcons/SocialIcons";
 import LogoTM from "../../../assets/logotm.svg";
 import TagLine from "../../../assets/tagline.svg";
 
+const FooterContainer = styled(Box)`
+  background-color: #1976d2;
+  color: white;
+`;
+
+const FooterContent = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+`;
+
+const LogoContainer = styled(Box)`
+  margin: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 600px) {
+    margin-bottom: -0.5rem;
+  }
+`;
+
+const LogoImage = styled.img`
+  width: 150px;
+`;
+
+const CopyrightText = styled(Typography)`
+  display: flex;
+  @media (max-width: 600px) {
+    font-size: 1rem;
+  }
+  align-self: end;
+  justify-content: center;
+  padding: 0 0 1rem 0;
+`;
+
 const Footer = () => {
   return (
-    <>
-      <Box sx={{ bgcolor: "#1976d2", color: "White" }}>
-        <Box
-          sx={{
-            display: "flex",
-            px: 2,
-            justifyContent: "space-between",
-            "@media (max-width: 600px)": { flexDirection: "column" },
-          }}
-        >
-          {/* Logo */}
-          <Box
-            sx={{
-              mt: 1,
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              "@media (max-width: 600px)": { p: 1 },
-            }}
-          >
-            <img src={LogoTM} alt="logotm" width={"150px"} />
-            <img src={TagLine} alt="tagline" />
-          </Box>
+    <FooterContainer>
+      <FooterContent>
+        {/* Logo */}
+        <LogoContainer>
+          <LogoImage src={LogoTM} alt="logotm" />
+          <img src={TagLine} alt="tagline" />
+        </LogoContainer>
 
-          {/* Social Icons */}
-          <SocialIcons sx={{ display: "flex", justifyContent: "center" }} />
-        </Box>
-        {/* Copyright */}
-        <Typography
-          variant="h8"
-          sx={{
-            display: "flex",
-            "@media (max-width: 600px)": { fontSize: "1rem" },
-            alignSelf: "end",
-            justifyContent: "center",
-            p: 1,
-          }}
-        >
-          All Rights Reserved &copy; Resto Ltd.
-        </Typography>
-      </Box>
-    </>
+        {/* Social Icons */}
+        <SocialIcons sx={{ display: "flex", justifyContent: "center" }} />
+      </FooterContent>
+
+      {/* Copyright */}
+      <CopyrightText variant="h8">
+        All Rights Reserved &copy; Resto Ltd.
+      </CopyrightText>
+    </FooterContainer>
   );
 };
 
