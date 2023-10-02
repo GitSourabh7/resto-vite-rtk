0ce782d2cbe6441dcdfd56c8a7516737d4bf9f63
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-10-02 15:36:58 +0530

    Add proptypes validation

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,19 @@
+import PropTypes from "prop-types";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <div>{children}</div>
+      <main>{children}</main>
       <Footer />
     </>
   );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired, // Ensure that children is a valid React node
 };
 
 export default Layout;
