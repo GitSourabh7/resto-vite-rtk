8860ab61e9f5fb0000f47c37fe8bf3ca30b223cf
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-10-02 11:45:12 +0530

    Update PageNotFound using Styled-components

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,18 +1,34 @@
+import styled from "styled-components";
 import Layout from "../components/layout/Layout";
 import { Box } from "@mui/material";
 import "@lottiefiles/lottie-player";
+
+// Styling for outer Box
+const StyledBox = styled(Box)`
+  display: flex;
+  justify-content: center;
+`;
+
+// Define a CSS class for styling the lottie-player
+const LottiePlayerWrapper = styled.div`
+  width: 680px;
+  display: flex;
+  justify-content: center;
+`;
+
 const PageNotFound = () => {
   return (
     <Layout>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <lottie-player
-          autoplay
-          loop
-          mode="normal"
-          src="https://lottie.host/e439b133-24af-43c6-b59b-434046fad0ad/MlOCktSwXL.json"
-          style={{ width: "680px" }}
-        ></lottie-player>
-      </Box>
+      <StyledBox>
+        <LottiePlayerWrapper>
+          <lottie-player
+            autoplay
+            loop
+            mode="normal"
+            src="https://lottie.host/e439b133-24af-43c6-b59b-434046fad0ad/MlOCktSwXL.json"
+          ></lottie-player>
+        </LottiePlayerWrapper>
+      </StyledBox>
     </Layout>
   );
 };
