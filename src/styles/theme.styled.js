b24efe44d29aa2916942e99b8f9d1ba434a48023
gitsourabh7 <sourabhnerlekar22@gmail.com>
2023-10-02 16:17:28 +0530

    Update theme

--- src/styles/theme.styled.js
+++ src/styles/theme.styled.js
@@ -1,5 +1,8 @@
 export const colors = {
   primaryColor: "#1976d2",
-  disabledColor: "#00000042",
+  transparentBlackColor: "#00000042",
   secondaryColor: "red",
+  whiteColor: "white",
 };
+
+export const backgroundGradient = `radial - gradient(circle, rgba(255, 255, 255, 0.1), transparent)`;
